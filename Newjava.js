11f4341d7e17df7205919368bdda15dbd5be3e74
Emmanuel <godlytone@gmail.com>
2022-11-08 05:25:40 +0100

    root

---
+++ Newjava.js
@@ -0,0 +1,3 @@
+document.getElementById('One').innerHTML="About Myself";
+document.getElementById('child').innerHTML="My our is name is Empire enterpries";
+document.getElementById('people').innerHTML="Contact us via Email godlytone@gmail.com and mobile phone +234";
